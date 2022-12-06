dfde1d06bfa9e48dc7430d29b9158ac62db82ef4
Robert M Newton <robert.m.newton7@gmail.com>
2022-12-06 17:00:28 +1000

    Update sprites

--- components/sprite.py
+++ components/sprite.py
@@ -3,9 +3,11 @@ from .component import Component
 
 
 class Sprite(Component):
-    def __init__(self, path_to_texture):
+    def __init__(self, path_to_texture, trans_c=None):
         super(Sprite, self).__init__()
 
         self.name = "sprite"
 
         self.surf = pygame.image.load(path_to_texture).convert()
+        if trans_c:
+            self.surf.set_colorkey(trans_c)
