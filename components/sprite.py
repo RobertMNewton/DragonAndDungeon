6236ffdb3e365a2d22a306ac107866ffbbe00f02
Robert M Newton <robert.m.newton7@gmail.com>
2022-12-06 16:02:42 +1000

    Create Sprite class

--- components/sprite.py
+++ components/sprite.py
@@ -1,8 +1,18 @@
-from pygame.sprite import Sprite
+import pygame
 from component import Component
 
 
-class Sprite(Component, Sprite):
-    def __init__(self):
-        pass
+class Sprite(Component):
+    def __init__(self, path_to_texture):
+        super(Sprite, self).__init__()
 
+        self.name = "sprite"
+
+        self.surf = pygame.image.load(path_to_texture).convert()
+        self.rect = self.surf.get_rect()
+
+    def get_surface(self):
+        return self.surf
+
+    def get_rect(self):
+        return self.rect
