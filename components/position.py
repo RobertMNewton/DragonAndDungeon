26ed2840470790c6a841650b91e45fe74fb89af9
Robert M Newton <robert.m.newton7@gmail.com>
2022-12-06 16:11:48 +1000

    Add Position Component

---
+++ components/position.py
@@ -0,0 +1,12 @@
+from component import Component
+class Position(Component):
+    def __init__(self, start_x=0, start_y=0):
+        super(Position, self).__init__()
+
+        self.name = "position"
+
+        self.x = start_x
+        self.y = start_y
+
+    def get_position(self):
+        return self.x, self.y
