e0ec64e33fcb358d5af3a21abe4fb2a3c443b518
Robert M Newton <robert.m.newton7@gmail.com>
2022-12-06 16:19:09 +1000

    Add velocity component

---
+++ components/velocity.py
@@ -0,0 +1,11 @@
+from component import Component
+
+
+class Velocity(Component):
+    def __init__(self, start_x=0, start_y=0):
+        super(Velocity, self).__init__()
+
+        self.name = "velocity"
+
+        self.x = start_x
+        self.y = start_y
