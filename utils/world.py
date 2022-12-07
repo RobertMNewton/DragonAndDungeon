994c84bda2dc8c2b14e052cc01dc45e5334ecbf5
Robert M Newton <robert.m.newton7@gmail.com>
2022-12-07 19:42:05 +1000

    Add Random Procedural World Generation

--- utils/world.py
+++ utils/world.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 from math import ceil, floor
 from entities.entity import Entity
 from components.position import Position
@@ -18,12 +18,22 @@ class World:
             absolute_y = pos1[1] + (16 * iy)
             for ix in range((ceil(pos2[0]) - floor(pos1[0])) // 16):
                 absolute_x = pos1[0] + (16 * ix)
-                data.append(self.create_tile(absolute_x, absolute_y, "assets/grassland_textures/grass_grassy.png"))
+                data.append(self.get_spot_data((absolute_x, absolute_y)))
         return data
 
     def get_spot_data(self, pos):
         x, y = pos
-        return self.create_tile(x, y, "assets/grassland_textures/grass_grassy.png")
+
+        random.seed(self.seed * x + y)
+        roll = random.random()
+        if roll < 0.75:
+            return self.create_tile(x, y, "assets/grassland_textures/grass_grassy.png")
+        elif roll < 0.85:
+            return self.create_tile(x, y, "assets/grassland_textures/grass_rock_small.png")
+        elif roll < 0.95:
+            return self.create_tile(x, y, "assets/grassland_textures/grass_rock_cluster_2.png")
+        else:
+            return self.create_tile(x, y, "assets/grassland_textures/grass_rock_cluster_1.png")
 
     def create_tile(self, x, y, texture):
         tile = Entity()
